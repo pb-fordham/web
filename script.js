49079f7df1fae1a956565ff614af0a6c7034bdab
pb-fordham <142920891+pb-fordham@users.noreply.github.com>
2023-08-24 13:56:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 $(document).ready(function(){
 
 
-  $("#container-header").load("_header.html"); 
+  $("#container-header").load("/_header.html"); 
   $("#sidebar").load("_sidebar.html"); 
   $("footer").load("_footer.html"); 
 
