4b063b4d37a5a95a3b24fa5e4717c774f4d6872e
pb-fordham <142920891+pb-fordham@users.noreply.github.com>
2023-08-24 13:58:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@
 $(document).ready(function(){
 
 
-  $("#container-header").load("_header.html"); 
-  $("#sidebar").load("_sidebar.html"); 
-  $("footer").load("_footer.html"); 
+  $("#container-header").load("https://pb-fordham.github.io/web/_header.html"); 
+  $("#sidebar").load("https://pb-fordham.github.io/web/_sidebar.html"); 
+  $("footer").load("https://pb-fordham.github.io/web/_footer.html"); 
 
 
  
